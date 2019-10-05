6d86842339cdd26f21f1bba02a249a86430fcb15
Sanketh Reddy <47980147+nsankethreddy@users.noreply.github.com>
2019-10-05 20:55:24 +0530

    update it with more functions

--- sol.sol
+++ sol.sol
@@ -7,17 +7,41 @@ contract TicTacToe {
     uint256 myUint256;
     string myString = "myString";
     bytes myBytes = "myString" ; 
+    uint[5] public myunitarray;
+    uint[3][3] board;
+    
+     struct mystruct{
+        uint counter;
+        address from;
+        bool isset;
+    }
+    
+    mystruct public somevalue;
     
     function TicTacToe() public{
         myaddress = msg.sender;
+        myunitarray[0]=1;
+        somevalue.counter = 5;
+        somevalue.from = msg.sender;
     }
-    function fundmycontact()public payable{
-    //this sends the ethers 
-        
+    function fundmycontact()public payable{    }
+    
+   
+    
+    function getboard() public view returns(uint[3][3]){
+        return board;
     }
+    
+    function getmyuintarray() public view returns(uint[5]){
+       
+    }
+    
+    function increase() public {
+        somevalue.counter++;
+    }
+    
     function withdraw()public{
         myaddress.transfer(this.balance );
-        //this reseives the ethers 
     }
     
     function setmybool(bool _myboolArg)  public{
@@ -27,7 +51,6 @@ contract TicTacToe {
     }
         
     function getmybool() public view returns(bool){
-        //this returns the boolean value
         return mybool;
     }
     
